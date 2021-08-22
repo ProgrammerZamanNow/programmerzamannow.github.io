e54e8f59f24f24ff611adb13f05e9ea008a5d8a1
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2021-08-22 19:10:35 +0700

    Add on progress

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ function HomepageHeader() {
     return (
         <header className={clsx('hero hero--primary', styles.heroBanner)}>
             <div className="container">
-                <h1 className="hero__title">{siteConfig.title}</h1>
+                <h1 className="hero__title">{siteConfig.title} | On Progress</h1>
                 <p className="hero__subtitle">{siteConfig.tagline}</p>
                 <div className={styles.buttons}>
                     <Link
@@ -29,7 +29,7 @@ export default function Home() {
     const {siteConfig} = useDocusaurusContext();
     return (
         <Layout
-            title={`${siteConfig.title} | On Progress`}
+            title={`${siteConfig.title}`}
             description="Tutorial Pemrograman, Backend, Frontend, Website, Mobile dan Devops Terlengkap di Indonesia<head />">
             <HomepageHeader/>
             <main>
